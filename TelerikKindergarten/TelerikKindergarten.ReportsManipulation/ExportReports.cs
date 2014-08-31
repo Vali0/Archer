671b0e23b7ce61f5115544ad97e2aa1ff7fbdbaa
Vali0 <valio_ch@mail.bg>
2014-08-31 15:12:49 +0300

    method for extracting zip files. method for creating basic pdf report

---
+++ TelerikKindergarten/TelerikKindergarten.ReportsManipulation/ExportReports.cs
@@ -0,0 +1,50 @@
+﻿namespace TelerikKindergarten.ReportsManipulation
+{
+    using MigraDoc.DocumentObjectModel;
+    using MigraDoc.Rendering;
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class ExportReports
+    {
+        public static void CreatePdfReport()
+        {
+            Document document = new Document();
+
+            Section section = document.AddSection();
+            var table = section.AddTable();
+            var column = table.AddColumn("5cm");
+            column = table.AddColumn("2cm");
+
+            var row = table.AddRow();
+            row.Cells[0].AddParagraph("Name");
+            row.Cells[1].AddParagraph("Age");
+            row.Cells[0].Borders.Visible = true;
+            row.Cells[1].Borders.Visible = true;
+
+            var firstRow = table.AddRow();
+            firstRow.Cells[0].AddParagraph("Pesho");
+            firstRow.Cells[1].AddParagraph("12");
+            firstRow.Cells[0].Borders.Visible = true;
+            firstRow.Cells[1].Borders.Visible = true;
+
+            var secondRow = table.AddRow();
+            secondRow.Cells[0].AddParagraph("Gosho");
+            secondRow.Cells[1].AddParagraph("50");
+            secondRow.Cells[0].Borders.Visible = true;
+            secondRow.Cells[1].Borders.Visible = true;
+
+
+            PdfDocumentRenderer renderer = new PdfDocumentRenderer(true, PdfSharp.Pdf.PdfFontEmbedding.Always);
+            renderer.Document = document;
+            renderer.RenderDocument();
+            string filename = "kindergarden-demo.pdf";
+            renderer.PdfDocument.Save(filename);
+            Process.Start(filename);
+        }
+    }
+}
---
+++ TelerikKindergarten/TelerikKindergarten.ReportsManipulation/ImportReports.cs
@@ -0,0 +1,34 @@
+﻿namespace TelerikKindergarten.ReportsManipulation
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.IO.Compression;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class ImportReports
+    {
+        public static void ExtractZipReports()
+        {
+            string zipPath = @"Excel import reports.zip";
+            string extractPath = @"extract";
+
+            using (ZipArchive archive = ZipFile.OpenRead(zipPath))
+            {
+                foreach (var entry in archive.Entries)
+                {
+                    if (entry.FullName.EndsWith(".xls", StringComparison.OrdinalIgnoreCase))
+                    {
+                        entry.ExtractToFile(Path.Combine(extractPath, entry.FullName));
+                    }
+                    else
+                    {
+                        Directory.CreateDirectory(Path.Combine(extractPath, entry.FullName));
+                    }
+                }
+            }
+        }
+    }
+}
