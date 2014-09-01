d62b521e8514fee705968677c4243d7f6be5a1d7
mTonkov <mtonkov@yahoo.com>
2014-09-01 14:43:19 +0300

    Export and Import of XML

--- TelerikKindergarten/TelerikKindergarten.ReportsManipulation/ImportReports.cs
+++ TelerikKindergarten/TelerikKindergarten.ReportsManipulation/ImportReports.cs
@@ -7,6 +7,8 @@
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
+    using System.Xml.Serialization;
+    using TelerikKindergarten.ReportModels;
 
     public class ImportReports
     {
@@ -30,5 +32,18 @@
                 }
             }
         }
+
+
+        public List<XmlReportViewModel> ReadXML(string filePath)
+        {
+            var collection = new List<XmlReportViewModel>();
+            var serializer = new XmlSerializer(collection.GetType());
+
+            using (var reader = new StreamReader(filePath))
+            {
+                return serializer.Deserialize(reader) as List<XmlReportViewModel>;
+            }
+        }
+
     }
 }
