3e623303525673f36c5fd1eeccdd25f0106c902e
theexar <easlavov@gmail.com>
2014-09-01 12:49:20 +0300

    Added SQLite models

---
+++ TelerikKindergarten/TelerikKindergarten.SQLite.Model/Diet.cs
@@ -0,0 +1,15 @@
+﻿namespace TelerikKindergarten.SQLite.Model
+{
+    using System.Collections.Generic;
+
+    public class Diet
+    {
+        public int DietID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public virtual ICollection<Menu> Menus { get; set; }
+    }
+}
---
+++ TelerikKindergarten/TelerikKindergarten.SQLite.Model/Menu.cs
@@ -0,0 +1,15 @@
+﻿namespace TelerikKindergarten.SQLite.Model
+{
+    using System.Collections.Generic;
+
+    public class Menu
+    {
+        public int MenuID { get; set; }
+
+        public virtual ICollection<Dish> Content { get; set; }
+
+        public string Notes { get; set; }
+
+        public virtual ICollection<Diet> AppropriateDiets { get; set; }
+    }
+}
