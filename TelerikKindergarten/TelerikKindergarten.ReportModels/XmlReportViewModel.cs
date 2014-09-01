e4cc6d4e32455708940464ca39930bbe92a1bd50
Vali0 <valio_ch@mail.bg>
2014-09-01 13:31:00 +0300

    report view models

--- TelerikKindergarten/TelerikKindergarten.ReportModels/XmlReportViewModel.cs
+++ TelerikKindergarten/TelerikKindergarten.ReportModels/XmlReportViewModel.cs
@@ -9,8 +9,9 @@
     public class XmlReportViewModel
     {
         public string Producer { get; set; }
-        public DateTime Date { get; set; }
-        public decimal TotalSum { get; set; }
 
+        public DateTime Date { get; set; }
+
+        public decimal TotalSum { get; set; }
     }
 }
