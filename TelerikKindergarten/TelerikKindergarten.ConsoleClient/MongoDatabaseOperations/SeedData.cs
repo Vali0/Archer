1ee6d1defbe8dc06f4199510ccf92c1c8a23b542
theexar <easlavov@gmail.com>
2014-09-03 17:23:06 +0300

    MongoDB seeding exported to separate class

---
+++ TelerikKindergarten/TelerikKindergarten.ConsoleClient/MongoDatabaseOperations/SeedData.cs
@@ -0,0 +1,90 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TelerikKindergarten.SQL.Model;
+
+namespace TelerikKindergarten.ConsoleClient.MongoDatabaseOperations
+{
+    public static class SeedData
+    {
+        public static void SeedMongoDb(string connectionString)
+        {
+            var client = new MongoClient(connectionString);
+            var server = client.GetServer();
+            var database = server.GetDatabase("test");
+
+            var producers = database.GetCollection<Producer>("producers");
+            producers.RemoveAll();
+            SeedProducers(producers);
+
+            var departments = database.GetCollection<Department>("departments");
+            departments.RemoveAll();
+            SeedDepartments(departments);
+
+            var groups = database.GetCollection<Group>("groups");
+            groups.RemoveAll();
+            SeedGroups(groups);
+        }
+
+        private static void SeedGroups(MongoCollection<Group> groups)
+        {
+            for (int indexOfGroups = 0; indexOfGroups < 20; indexOfGroups++)
+            {
+                var currentGroup = new Group() { Name = "G " + indexOfGroups, Notes = "Notes" };
+
+                for (int i = 0; i < 10; i++)
+                {
+                    var currentStudent = new Child() { ConclusionDate = DateTime.Now, LastName = "Child" + i + " " + indexOfGroups, Address = "Sofia?" };
+                    currentGroup.Children.Add(currentStudent);
+                }
+
+                var teacher = new Employee() { LastName = "Name" + indexOfGroups };
+                currentGroup.Employee = teacher;
+                groups.Insert(currentGroup);
+            }
+        }
+
+        private static void SeedDepartments(MongoCollection<Department> departments)
+        {
+            for (int indexOfDepartments = 0; indexOfDepartments < 5; indexOfDepartments++)
+            {
+                var currentDepartment = new Department() { Name = "Dept." + indexOfDepartments };
+                currentDepartment.DepartmentHead = new Employee() { FirstName = "Department head of " + currentDepartment.Name };
+
+                for (int i = 0; i < 10; i++)
+                {
+                    var employee = new Employee() { MiddleName = currentDepartment.Name + " Jonny " + i };
+                    currentDepartment.Employees.Add(employee);
+                }
+
+                for (int i = 0; i < 10; i++)
+                {
+                    var asset = new Asset() { Description = "Descr" + i, Value = i * 5, AssetType = new AssetType() { Name = "Chair" } };
+                    currentDepartment.Assets.Add(asset);
+                }
+
+                departments.Insert(currentDepartment);
+            }
+        }
+
+        private static void SeedProducers(MongoCollection<Producer> producers)
+        {
+            for (int indexOfProducers = 0; indexOfProducers < 20; indexOfProducers++)
+            {
+                var currentProducer = new Producer() { Name = "Producer" + indexOfProducers };
+
+                for (int i = 0; i < 30; i++)
+                {
+                    var currentProduct = new Product() { Name = "Product " + currentProducer.Name + " " + i, Price = i, Quantity = i * 10 };
+
+                    currentProducer.Products.Add(currentProduct);
+                }
+
+                producers.Insert(currentProducer);
+            }
+        }
+    }
+}
--- TelerikKindergarten/TelerikKindergarten.ConsoleClient/Program.cs
+++ TelerikKindergarten/TelerikKindergarten.ConsoleClient/Program.cs
@@ -13,11 +13,19 @@
     using TelerikKindergarten.SQLite.Data;
     using MongoDB.Driver;
     using TelerikKindergarten.ConsoleClient.SQL;
+    using TelerikKindergarten.ConsoleClient.MongoDatabaseOperations;
 
     public class Program
     {
         public static void Main(string[] args)
         {
+            //Mongodb seeding
+            string mongoConnectionString = "mongodb://localhost";
+            //Get data from the base.
+            var client = new MongoClient(mongoConnectionString);
+            var server = client.GetServer();
+            var database = server.GetDatabase("test");
+            //SeedData.SeedMongoDb(mongoConnectionString); // Uncomment to seed mongodb
             
             // Load Excel Reports from ZIP File
             //var importedExcelReports = ExcelManipulator.Import();
@@ -41,97 +49,14 @@
             // Excel data
             //ExcelManipulator.Export();
 
-            //Mongodb seeding
-            string mongoConnectionString = "mongodb://localhost";
-            //Get data from the base.
-            var client = new MongoClient(mongoConnectionString);
-            var server = client.GetServer();
-            var database = server.GetDatabase("test");
-            //SeedMongoDb(mongoConnectionString); // Uncomment to seed mongodb
+            
 
             // SQL seeding
             var sqlContext = new TelerikKindergartenData();
             //SeedSql.SeedSqlWithData(sqlContext, database); // Uncomment to seed sql
 
             //UpdateMySql();
-        }
-
-        private static void SeedMongoDb(string connectionString)
-        {
-            var client = new MongoClient(connectionString);
-            var server = client.GetServer();
-            var database = server.GetDatabase("test");
-
-            var producers = database.GetCollection<Producer>("producers");
-            producers.RemoveAll();
-            SeedProducers(producers);
-
-            var departments = database.GetCollection<Department>("departments");
-            departments.RemoveAll();
-            SeedDepartments(departments);
-
-            var groups = database.GetCollection<Group>("groups");
-            groups.RemoveAll();
-            SeedGroups(groups);
-        }
-
-        private static void SeedGroups(MongoCollection<Group> groups)
-        {
-            for (int indexOfGroups = 0; indexOfGroups < 20; indexOfGroups++)
-            {
-                var currentGroup = new Group() { Name = "G " + indexOfGroups, Notes = "Notes" };
-
-                for (int i = 0; i < 10; i++)
-                {
-                    var currentStudent = new Child() { ConclusionDate = DateTime.Now, LastName = "Child" + i + " " + indexOfGroups, Address = "Sofia?" };
-                    currentGroup.Children.Add(currentStudent);
-                }
-
-                var teacher = new Employee() { LastName = "Name" + indexOfGroups };
-                currentGroup.Employee = teacher;
-                groups.Insert(currentGroup);
-            }
-        }
-
-        private static void SeedDepartments(MongoCollection<Department> departments)
-        {
-            for (int indexOfDepartments = 0; indexOfDepartments < 5; indexOfDepartments++)
-            {
-                var currentDepartment = new Department() { Name = "Dept." + indexOfDepartments };
-                currentDepartment.DepartmentHead = new Employee() { FirstName = "Department head of " + currentDepartment.Name };
-
-                for (int i = 0; i < 10; i++)
-                {
-                    var employee = new Employee() { MiddleName = currentDepartment.Name + " Jonny " + i };
-                    currentDepartment.Employees.Add(employee);
-                }
-
-                for (int i = 0; i < 10; i++)
-                {
-                    var asset = new Asset() { Description = "Descr" + i, Value = i * 5, AssetType = new AssetType() { Name = "Chair" } };
-                    currentDepartment.Assets.Add(asset);
-                }
-
-                departments.Insert(currentDepartment);
-            }
-        }
-
-        private static void SeedProducers(MongoCollection<Producer> producers)
-        {
-            for (int indexOfProducers = 0; indexOfProducers < 20; indexOfProducers++)
-            {
-                var currentProducer = new Producer() { Name = "Producer" + indexOfProducers };
-
-                for (int i = 0; i < 30; i++)
-                {
-                    var currentProduct = new Product() { Name = "Product " + currentProducer.Name + " " + i, Price = i, Quantity = i * 10 };
-
-                    currentProducer.Products.Add(currentProduct);
-                }
-
-                producers.Insert(currentProducer);
-            }
-        }
+        }        
 
         private static void UpdateMySql()
         {
